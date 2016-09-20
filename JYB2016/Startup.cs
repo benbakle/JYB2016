687b006cd1ba367fd1517a1c94fe8bbeaef3dca3
Benjamin Bakle <bbakle@gmail.com>
2016-09-19 20:32:01 -0400

    Initial Commit

---
+++ JYB2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JYB2016.Startup))]
+namespace JYB2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
